770cd9a46c7f63ed40f48d046eb8c6fb0aef8fb1
bbennett7 <bryn.bennett@gmail.com>
2019-07-16 19:08:12 -0700

    Create directories and add redux as dependency

--- wikitrash-rails/db/migrate/20190716225652_create_items.rb
+++ wikitrash-rails/db/migrate/20190716225652_create_items.rb
@@ -6,9 +6,11 @@ class CreateItems < ActiveRecord::Migration[5.2]
       t.string :image
       t.text :rules
       t.text :locations
+      t.text :sources
       t.boolean :verified
       t.integer :downvotes
       t.integer :upvotes
+      t.integer :searches
       t.timestamps
     end
   end
