38c393ed300511c3fa527359c628900338cf20be
bbennett7 <bryn.bennett@gmail.com>
2019-07-17 22:19:17 -0700

    Add handleonchange to search bar

--- wikitrash-react/src/components/SearchBar.js
+++ wikitrash-react/src/components/SearchBar.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+  constructor() {
+    super();
+    this.state = {
+      searchInput: ''
+    }
+  }
+
+  handleOnChange = (event) => {
+    this.setState({
+      searchInput: event.target.value
+    })
+    console.log(this.state.searchInput)
+  }
+
   render() {
     return (
       <form>
-      <label> Search <input type="text" /> </label>
+      <label> Search <input type="text" onChange={this.handleOnChange} value={this.state.searchInput} /> </label>
       </form>
     )
   }
--- wikitrash-react/src/containers/VerifiedContainer.js
+++ wikitrash-react/src/containers/VerifiedContainer.js
@@ -2,12 +2,7 @@ import React, { Component } from 'react';
 import SearchBar from '../components/SearchBar'
 
 export default class UnverifiedContainer extends Component {
-  constructor() {
-    super();
-    this.state = {
-      searchInput: ''
-    }
-  }
+
   render() {
     return (
       <div>
