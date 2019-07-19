12a5cc982d1e6278698f9fa6286978b1359ec673
bbennett7 <bryn.bennett@gmail.com>
2019-07-18 23:09:24 -0700

    Style header

--- wikitrash-react/src/App.js
+++ wikitrash-react/src/App.js
@@ -4,18 +4,19 @@ import './App.css';
 import NavBar from './components/NavBar'
 import { connect } from 'react-redux'
 import { fetchItems } from './actions/items'
+import RecyclingSymbol from './images/RecyclingSymbol.jpg'
 
 class App extends Component {
 
   componentDidMount() {
     this.props.fetchItems()
   }
-
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          wikitrash
+          <span className="Header">WikiTr <img className="R-symbol" src={RecyclingSymbol} height="50px" width="50px" /> sh</span>
+          <p>Your source for the responsible disposal of all materials.</p>
         </header>
         <NavBar />
       </div>
