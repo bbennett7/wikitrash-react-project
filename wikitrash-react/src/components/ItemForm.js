6c9d9327cb703ac41983e1fec87c3cb8b364a9e5
bbennett7 <bryn.bennett@gmail.com>
2019-07-19 00:08:48 -0700

    Style add form

--- wikitrash-react/src/components/ItemForm.js
+++ wikitrash-react/src/components/ItemForm.js
@@ -43,17 +43,20 @@ class ItemForm extends Component {
   render() {
     return (
       <div className="Main">
-        <form onSubmit={this.handleOnSubmit}>
-            <h3>add an item</h3>
+        <h3>add an item</h3>
+        <form className="Add-form" onSubmit={this.handleOnSubmit}>
           <label> Item <input type="text" name="name" onChange={this.handleOnChange} value={this.state.name} /> </label> <br />
-            <label> Recyclable?
-              <label> Yes <input type="radio" name="recyclable" onChange={this.handleOnChange} value="true" /> </label>
-              <label> No <input type="radio" name="recyclable" onChange={this.handleOnChange} value="false" /> </label>
-            </label> <br />
-          <label> Image <input type="text" name="image" onChange={this.handleOnChange} value={this.state.image} /> </label> <br />
-          <label> Rules <input type="textarea" name="rules" onChange={this.handleOnChange} value={this.state.rules} /> </label> <br />
-          <label> Locations (separated by semicolon) <input type="textarea" name="locations" onChange={this.handleOnChange} value={this.state.locations} /> </label> <br />
-          <label> References (separated by semicolon) <input type="textarea" name="references" onChange={this.handleOnChange} value={this.state.references} /> </label> <br />
+          <label> Recyclable?
+            Yes <input type="radio" name="recyclable" onChange={this.handleOnChange} value="true" />
+            No <input type="radio" name="recyclable" onChange={this.handleOnChange} value="false" />
+          </label> <br />
+          <label className="Image-input"> Image <input type="text" className="Image-input" name="image" onChange={this.handleOnChange} value={this.state.image} /> </label><br />
+          <label> Rules <br/>
+          <textarea name="rules" onChange={this.handleOnChange} value={this.state.rules} /> </label> <br />
+          <label> Locations (separated by semicolon) <br/>
+          <textarea name="locations" onChange={this.handleOnChange} value={this.state.locations} /> </label> <br />
+          <label> References (separated by semicolon) <br/>
+          <textarea name="references" onChange={this.handleOnChange} value={this.state.references} /> </label> <br />
           <input type="submit" value="Add item"/>
         </form>
       </div>
