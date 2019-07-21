4a56f15d3926b31e5933b89638b4c9b9ffb2d90b
bbennett7 <bryn.bennett@gmail.com>
2019-07-20 17:22:38 -0700

    Fix error received when search button is clicked and empty

--- wikitrash-react/src/reducers/manageItems.js
+++ wikitrash-react/src/reducers/manageItems.js
@@ -11,8 +11,10 @@ export default function manageItems(state = {loading: false, items: [], searchIt
       return {...state, items: [...state.items, Object.assign(action.item, {verified: false, downvotes: 0, upvotes: 0})]};
 
     case 'SEARCH_ITEMS':
-      const item = state.items.filter((item) => item.name.toLowerCase() === action.payload.toLowerCase())
-      item[0].searches += 1
+      let item = []
+      item = state.items.filter((item) => item.name.toLowerCase() === action.payload.toLowerCase())
+      console.log(item)
+      if (item.length > 0) { item[0].searches += 1 }
       return {...state, searchItem: Object.assign(item)};
 
     case 'RESET_SEARCH_ITEM':
@@ -34,11 +36,8 @@ export default function manageItems(state = {loading: false, items: [], searchIt
       console.log(downVoteItem[0].downvotes)
       if (downVoteItem[0].downvotes >= 10) {
         state.items.splice(index, 1)
-        return {...state};
-      } else {
-        return {...state};
-      }
-
+      };
+      return {...state};
     default:
       return state;
   }
